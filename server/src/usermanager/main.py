66b5f40ec528a4b206fcc61e821df90aa0723493
KacDabQ <u118_kacdab_waw@technischools.com>
2025-03-29 15:47:40 +0100

    users by id

--- server/src/usermanager/main.py
+++ server/src/usermanager/main.py
@@ -1,28 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 class Person():
-    def __init__(self, name:str, age:int):
-        self.name = name
+    def __init__(self, id:int, firstname:str, lastname:str, age:int, group:str):
+        self.id = id
+        self.firstname = firstname
+        self.lastname = lastname
         self.age = age
+        self.group = group
+
 
     def to_json(self):
         return {    
-            "name": self.name,
-            "age": self.age
+            "id": self.id,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "age": self.age,
+            "group": self.group
         }
 
-    '''{ "id": 4, "name": "Wojciech", "lastname": "Oczkowski" },
-    { "id": 3, "name": "Jan", "lastname": "Kowalski" },
-    { "id": 2, "name": "Adam", "lastname": "Nowak" },
-    { "id": 1, "name": "Marta", "lastname": "Nowakowska"}'''
 
 
 
 
 
-users = [Person("Kacper", 16), Person("Kamil", 14), Person("Kendrick", 20)]
+users = [Person(1, "John", "Doe", 25, "admin"), Person(2, "Jane", "Doe", 22, "user")]
 
 
 
@@ -30,16 +33,21 @@ users = [Person("Kacper", 16), Person("Kamil", 14), Person("Kendrick", 20)]
 
 @app.route("/")
 def main_menu():
-    return "<h1>Hello world!</h1>"
+    return "<h1>Hello world!</h1>", 200
 
 @app.route("/users", methods=['GET'])
 def get_all_users():
-    return [u.to_json() for u in users]
+    return [u.to_json() for u in users], 200
 
-@app.route("/test/<username>/<int:age>")
-def test(username, age):
-    users = [Person(username, age)] * 10
-    return [u.to_json() for u in users]
+@app.route("/users/<int:id>")
+def get_all_users_by_id(id):
+    filtered_users = []
+    for u in users:
+        if u.id == id:
+            filtered_users.append(u)
+    if len(filtered_users) == 0:
+        return "User not found", 404
+    return [u.to_json() for u in filtered_users], 200
 
 def start():
     app.run(debug=True)
